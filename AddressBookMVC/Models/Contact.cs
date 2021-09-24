adcb15c0fecc8f7d617048e6daf04d3104947564
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2021-09-24 04:23:06 -0400

    Initial commit

---
+++ AddressBookMVC/Models/Contact.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AddressBookMVC.Models
+{
+    public class Contact
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# AddressBookMVC
+ AddressBook MVC Application created during Coder Foundry Online Bootcamp using Visual Studio 2019 on Windows 10
